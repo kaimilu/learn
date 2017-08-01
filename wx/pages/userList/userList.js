2b2bc1f538267cc596443e6dd630b5f32d299164
suzhihui <wisdom.su@163.com>
2017-08-01 17:48:35 +0800

    init wx

---
+++ wx/pages/userList/userList.js
@@ -0,0 +1,107 @@
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        imgUrls: [
+            'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+            'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+            'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+        ],
+        // 是否显示面板指示点
+        indicatorDots: true,
+        // 是否自动切换
+        autoplay: true,
+        // 自动切换时间间隔
+        interval: 3000,
+        // 滑动动画时长
+        duration: 1000
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
+        var userList = [{
+            "userName": "老苏",
+            "juli": "1km",
+            "live": "Lv.5",
+            "yz": "已验证",
+            "js": "男神",
+            "desc": "比比纟比比"
+        }]
+        this.setData(userList)
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
+
+    },
+
+    changeIndicatorDots: function(e) {
+        this.setData({
+            indicatorDots: !this.data.indicatorDots
+        })
+    },
+    changeAutoplay: function(e) {
+        this.setData({
+            autoplay: !this.data.autoplay
+        })
+    },
+    intervalChange: function(e) {
+        this.setData({
+            interval: e.detail.value
+        })
+    },
+    durationChange: function(e) {
+        this.setData({
+            duration: e.detail.value
+        })
+    }
+
+})
