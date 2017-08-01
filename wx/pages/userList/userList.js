af693778b8f07c8f7ca154e2d0ca85b667c35413
suzhihui <wisdom.su@163.com>
2017-08-01 22:48:33 +0800

    事件冒泡

--- wx/pages/userList/userList.js
+++ wx/pages/userList/userList.js
@@ -37,8 +37,17 @@ Page({
             "yz": "已验证",
             "js": "男神",
             "desc": "比比纟比比"
+        },{
+           "userName": "老苏1",
+            "juli": "1km",
+            "live": "Lv.5",
+            "yz": "已验证",
+            "js": "男神",
+            "desc": "比比纟比比" 
         }]
-        this.setData(userList)
+        this.setData({
+            user_list:userList
+        })
     },
 
     /**
