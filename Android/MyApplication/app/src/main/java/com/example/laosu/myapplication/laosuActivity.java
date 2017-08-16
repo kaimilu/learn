e83857e39a959dc318623046e92e3ee72c277efa
suzhihui <wisdom.su@163.com>
2017-08-16 17:59:24 +0800

    add canvas demo

---
+++ Android/MyApplication/app/src/main/java/com/example/laosu/myapplication/laosuActivity.java
@@ -0,0 +1,13 @@
+package com.example.laosu.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class laosuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_laosu);
+    }
+}
