15a9c0b73a65d3cc6ba62acbc05114df8e503636
suzhihui <wisdom.su@163.com>
2017-08-25 18:03:07 +0800

    add file

--- blog/admin/src/main.js
+++ blog/admin/src/main.js
@@ -1,8 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
 import router from './router'
+
+import {sync} from 'vuex-router-sync'
+import store from './store/index'
+
+sync(store, router)
+
+import App from './App'
 
 Vue.config.productionTip = false
 
@@ -10,6 +16,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App }
 })
